bd4d11a5727492846f51da2d2a3e3e67c088e10e
June Jung <jeongjueun@gmail.com>
2012-07-11 16:37:30 -0700

    update from calc

--- calculator.rb
+++ calculator.rb
@@ -4,5 +4,10 @@ class Calculator
     @orig_address = orig_address
     @dest_address = dest_address
   end
+  def sending_info
+    # @trip = Trip.new(@orig_address, @dest_address)
+  end
+  def
+  end
 
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -12,4 +12,29 @@ describe "#initialize" do
     @test.orig_address.should == "717 california st,sf"
     @test.dest_address.should == "25 duboce ave,sf"
   end
+
 end
+
+describe "#sending_info" do
+  before :each do
+    @test = Calculator.new("717 california st,sf", "25 duboce ave,sf")
+    @test.sending_info
+  end
+
+  it "sends infomation to trip class" do
+    @test.should respond_to(:sending_info)
+  end
+end
+
+# describe "#" do
+#   before :each do
+#     @test = Calculator.new("717 california st,sf", "25 duboce ave,sf")
+#     @test.sending_info
+#   end
+#
+#   it "sends infomation to trip class" do
+#     @test.should respond_to(:sending_info)
+#   end
+
+end
+
